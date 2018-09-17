6f19eabfa95865c3e655bd835165fbcd437eb13b
Mike Molenda <pair+michael.molenda@cna.com>
2018-09-17 16:03:46 -0500

    Create Response class, return error message

--- src/main/kotlin/com/cna/gcrud/GcrudController.kt
+++ src/main/kotlin/com/cna/gcrud/GcrudController.kt
@@ -11,18 +11,22 @@ class GcrudController {
     private val datastore = DatastoreOptions.getDefaultInstance().service
 
     @RequestMapping(value = ["/create"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun createEntity(@RequestBody(required = true) task: Task): Task {
-        val keyFactory = datastore.newKeyFactory().setKind("Task")
-        val taskKey = datastore.allocateId(keyFactory.newKey())
-        val taskEntity = Entity
-                .newBuilder(taskKey)
-                .set("category", task.category)
-                .set("priority", task.priority)
-                .set("description", task.description)
-                .set("done", task.done)
-                .build()
-        datastore.put(taskEntity)
-        return task
+    fun createEntity(@RequestBody(required = true) task: Task): Response {
+        try {
+            val keyFactory = datastore.newKeyFactory().setKind("Task")
+            val taskKey = datastore.allocateId(keyFactory.newKey())
+            val taskEntity = Entity
+                    .newBuilder(taskKey)
+                    .set("category", task.category)
+                    .set("priority", task.priority)
+                    .set("description", task.description)
+                    .set("done", task.done)
+                    .build()
+            datastore.put(taskEntity)
+        } catch (e: Exception) {
+            return Response("Create task error", e)
+        }
+        return Response("Create task success", task)
     }
 
 }
@@ -32,4 +36,9 @@ data class Task(
         var priority: Long,
         var description: String,
         var done: Boolean = false
+)
+
+data class Response(
+        val message: String,
+        val payload: Any
 )
